38729bbc50d4b0b8fd5905223636ac87cbcdc249
gotlr98 <gotlr98@naver.com>
2023-03-31 17:36:07 +0900

    fix : fix Drink struct init & grid border

--- My_Project/My_Project/Drink_Info.swift
+++ My_Project/My_Project/Drink_Info.swift
@@ -8,31 +8,39 @@
 import SwiftUI
 
 struct Drink: Hashable, Identifiable{
+    
+    enum drink_type{
+        case soju, beer, korean_wine
+    }
+    
     var id: UUID
     var pruduct_name: String
     var price: Int64
+    var type: drink_type
+    
+    
 }
 
 struct Drink_Info: View {
     
     @State private var drinks = [
-        Drink(id: UUID(), pruduct_name: "cham", price: 1350),
-        Drink(id: UUID(), pruduct_name: "cass", price: 1500)
+        Drink(id: UUID(), pruduct_name: "cham", price: 1350, type: Drink.drink_type.soju),
+        Drink(id: UUID(), pruduct_name: "cass", price: 1500, type: Drink.drink_type.beer)
     ]
     
     let columns = [
-        GridItem(.adaptive(minimum: 100)),
-        GridItem(.adaptive(minimum: 100))
+        GridItem(.flexible()),
+        GridItem(.flexible())
     ]
     
     var body: some View {
-        ScrollView(){
+        ScrollView{
             LazyVGrid(columns: columns, alignment: .center, spacing: 30, content:{
                     ForEach(drinks) { drink in
                         VStack{
                             Image(drink.pruduct_name)
                                 .resizable()
-                                .frame(width:50, height: 50)
+                                .frame(width:70, height: 70)
                             HStack{
                                 Text(drink.pruduct_name)
                                 Text(String(drink.price))
@@ -40,7 +48,14 @@ struct Drink_Info: View {
                         }
                         
                     }
+                    .padding()
+                    .overlay(
+                            RoundedRectangle(cornerRadius: 16)
+                                .stroke(Color.blue, lineWidth: 4)
+                        )
             })
+            .padding()
+            
             
         }
         
