f39d49c2b03924cc17576a1337df738fd9df2632
gotlr98 <gotlr98@naver.com>
2023-04-13 13:44:29 +0900

    try check is url valid

--- My_Project/My_Project/View/Register_Drink.swift
+++ My_Project/My_Project/View/Register_Drink.swift
@@ -7,6 +7,26 @@
 
 import SwiftUI
 import Combine
+import Kingfisher
+
+func check_url_valid(url: String) -> Bool{
+    var isVaild: Bool = false
+    
+    
+    KFImage(URL(string: url))
+        .placeholder{
+            Color.gray
+        }
+        .retry(maxCount: 5, interval: .seconds(5))
+        .onSuccess{ s in
+            isVaild = true
+        }
+        .onFailure{ e in
+            isVaild = false
+        }
+    
+    return isVaild
+}
 
 struct Register_Drink: View {
     
@@ -15,6 +35,8 @@ struct Register_Drink: View {
     @State var input_name: String = ""
     @State var input_price: String = ""
     @State var input_img_url: String = ""
+    @State var show_alert: Bool = false
+    @State var is_url_valid: Bool = false
     @Binding var drink: [Drink]
     @Binding var show_sheet: Bool
     
@@ -44,7 +66,7 @@ struct Register_Drink: View {
                 .padding()
                 .background(Color(uiColor: .secondarySystemBackground))
                 .textContentType(.telephoneNumber)
-                .keyboardType(.numberPad)
+                .keyboardType(.numbersAndPunctuation)
                 
                 TextField(text: $input_img_url, label: {
                     Text("이미지 링크")
@@ -58,17 +80,35 @@ struct Register_Drink: View {
             
             VStack{
                 Button(action: {
-                    show_sheet.toggle()
-                    drink.append(Drink(id: UUID(), name: input_name, type: selected_type, price: Int64(input_price)!, img_url: input_img_url))
-                    for i in get_All_Drink(){
-                        print(i)
+//                    KFImage(URL(string: input_img_url))
+//                        .onSuccess {r in //성공
+//                            is_url_valid = true
+//                            print("success \(r)")
+//                        }
+//                        .onFailure { e in //실패
+//                            show_alert = true
+//                            print("failure: \(e)")
+//                        }
+
+                    if check_url_valid(url: input_img_url){
+                        drink.append(Drink(id: UUID(), name: input_name, type: selected_type, price: Int64(input_price)!, img_url: input_img_url))
+                        for i in get_All_Drink(){
+                          print(i)
+                        }
+                        switch selected_type {
+                        case .makgeolli:
+                            set_drink(name: input_name, price: Int64(input_price)!, drink_type: "makgeolli", img_url: input_img_url)
+                        case .spirits:
+                            set_drink(name: input_name, price: Int64(input_price)!, drink_type: "spirits", img_url: input_img_url)
+                        }
+                        show_sheet.toggle()
                     }
-                    switch selected_type {
-                    case .makgeolli:
-                        set_drink(name: input_name, price: Int64(input_price)!, drink_type: "makgeolli", img_url: input_img_url)
-                    case .spirits:
-                        set_drink(name: input_name, price: Int64(input_price)!, drink_type: "spirits", img_url: input_img_url)
+                    else{
+                        show_alert = true
                     }
+                    
+                    
+  
                 }, label: {
                     Rectangle()
                         .frame(width: 80, height: 50)
@@ -79,8 +119,12 @@ struct Register_Drink: View {
                                 .foregroundColor(Color.black)
                         }
                 })
+                .alert("이미지 URL을 확인해주세요", isPresented: $show_alert){
+                    Button("OK", role: .cancel){}
+                }
             }
             .position(x: geo.size.width / 2, y: geo.size.height / 1.6)
+
                 
             
         }
