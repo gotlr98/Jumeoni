a4559e7914980c1438c3908d86c10bbdf09d0721
gotlr98 <gotlr98@naver.com>
2023-05-05 13:12:35 +0900

    feat : add spirits review view

---
+++ My_Project/My_Project/View/Spirits_Review_View.swift
@@ -0,0 +1,20 @@
+//
+//  Spirits_Review_View.swift
+//  My_Project
+//
+//  Created by HaeSik Jang on 2023/05/05.
+//
+
+import SwiftUI
+
+struct Spirits_Review_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Spirits_Review_View_Previews: PreviewProvider {
+    static var previews: some View {
+        Spirits_Review_View()
+    }
+}
