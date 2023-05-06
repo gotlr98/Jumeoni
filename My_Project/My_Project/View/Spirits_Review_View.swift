bf4598f1023e2ac838d026d77a4d578437dfceb0
gotlr98 <gotlr98@naver.com>
2023-05-06 23:01:45 +0900

    spirits_review_view

--- My_Project/My_Project/View/Spirits_Review_View.swift
+++ My_Project/My_Project/View/Spirits_Review_View.swift
@@ -1,20 +1,144 @@
 //
-//  Spirits_Review_View.swift
+//  Makgeolli_Review_View.swift
 //  My_Project
 //
-//  Created by HaeSik Jang on 2023/05/05.
+//  Created by HaeSik Jang on 2023/04/18.
 //
 
 import SwiftUI
+import Cosmos
+import RealmSwift
 
-struct Spirits_Review_View: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct Spirits_Review_View: UIViewRepresentable {
+    @Binding var rating: Double
+    
+
+    func makeUIView(context: Context) -> CosmosView {
+        CosmosView()
+    }
+
+    func updateUIView(_ uiView: CosmosView, context: Context) {
+        uiView.rating = rating
+    
+        // Autoresize Cosmos view according to it intrinsic size
+        uiView.setContentHuggingPriority(.defaultHigh, for: .vertical)
+        uiView.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+      
+        // Change Cosmos view settings here
+        uiView.settings.starSize = 40
+        uiView.didFinishTouchingCosmos = { rating in
+            self.rating = rating
+        }
+    }
+}
+
+struct Makgeolli_Review_View: View {
+    
+    @EnvironmentObject var kakao: Kakao_AuthVM
+    @Binding var show_sheet: Bool
+    @State var show_alert: Bool = false
+    
+    
+    @State var sweet = 1.0
+    @State var bitter = 1.0
+    @State var sour = 1.0
+    @State var refreshing = 1.0
+    @State var thick = 1.0
+    @State var rating = 1.0
+    @State var comment = ""
+    
+    var drink: Drink
+    
+    var body: some View{
+        
+        VStack{
+            HStack{
+                Text("단맛")
+                    .font(.title)
+                    .padding(.all)
+                MyCosmosView(rating: $sweet)
+            }
+            
+            HStack {
+                Text("쓴맛")
+                    .font(.title)
+                    .padding(.all)
+                MyCosmosView(rating: $bitter)
+            }
+            
+            HStack {
+                Text("신맛")
+                    .font(.title)
+                    .padding(.all)
+                MyCosmosView(rating: $sour)
+            }
+            
+            HStack {
+                Text("청량감")
+                    .font(.title)
+                    .padding(.all)
+                MyCosmosView(rating: $refreshing)
+            }
+            
+            HStack {
+                Text("걸쭉함")
+                    .font(.title)
+                    .padding(.all)
+                MyCosmosView(rating: $thick)
+            }
+            Divider()
+            
+            HStack {
+                Text("총점")
+                    .font(.title)
+                    .padding(.all)
+                MyCosmosView(rating: $rating)
+            }
+            
+            HStack(alignment: .center) {
+                Text("한줄평")
+                    .font(.title)
+                    .padding(.all)
+                TextField(text: $comment, label:{
+                    Text("한줄평 입력해주세요")
+                })
+                .frame(width: 250)
+                .textInputAutocapitalization(.never)
+            }
+            
+            Button(action: {
+                print(kakao.user_name + "님")
+                
+                if comment.isEmpty{
+                    show_alert = true
+                }
+                else{
+                    let makgeolli_review = Makgeolli_Review(name: kakao.user_name, drink_name: drink.name, sweet: sweet, bitter: bitter, sour: sour, refreshing: refreshing, thick: thick, rating: rating, comment: comment, objectID: ObjectId())
+                    set_Makgeolli_Review(name: kakao.user_name, drink_name: drink.name, sweet: sweet, bitter: bitter, sour: sour, refreshing: refreshing, thick: thick, rating: rating, comment: comment)
+                    kakao.add_user_review(review: makgeolli_review)
+                    show_sheet.toggle()
+                }
+                
+            }, label: {
+                Rectangle()
+                    .frame(width: 120, height: 50)
+                    .foregroundColor(Color.secondary)
+                    .overlay{
+                        Text("리뷰 등록하기")
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.black)
+                    }
+            })
+            .alert("한줄평 입력해주세요", isPresented: $show_alert){
+                Button("OK", role: .cancel){}
+            }
+        }
+        
     }
 }
 
 struct Spirits_Review_View_Previews: PreviewProvider {
     static var previews: some View {
-        Spirits_Review_View()
+        Makgeolli_Review_View(show_sheet: .constant(false), drink: Drink(id: UUID(), name: "", type: .makgeolli, price: 12, img_url: ""))
     }
 }
