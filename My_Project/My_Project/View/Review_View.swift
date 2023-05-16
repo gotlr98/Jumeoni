ba21e8dfdb02f22f39f5a478fd1ad266503d6386
gotlr98 <gotlr98@naver.com>
2023-05-16 22:58:15 +0900

    basic database set

--- My_Project/My_Project/View/Review_View.swift
+++ My_Project/My_Project/View/Review_View.swift
@@ -24,7 +24,6 @@ struct Review_View: View {
             
             Text(drink.name + "리뷰")
            
-            
             List{
                 if selected_type == .makgeolli{
                     ForEach(makgeolli_review, id: \.self){ review in
